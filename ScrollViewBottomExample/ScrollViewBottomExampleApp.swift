a8b40535a0d1126c4f9d0f7b650881b380faf5c1
OvershotHavik <plavetzky@gmail.com>
2022-01-14 16:03:36 -0500

    Initial Commit

---
+++ ScrollViewBottomExample/ScrollViewBottomExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewBottomExampleApp.swift
+//  ScrollViewBottomExample
+//
+//  Created by Steve Plavetzky on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewBottomExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
